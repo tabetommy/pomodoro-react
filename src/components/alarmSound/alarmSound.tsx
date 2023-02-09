2c4d6656f69f19cfb4e1d5cf70f6e0d9f986642d
Tommy Egbe <tabe_e@yahoo.ca>
2023-02-09 21:19:39 +0100

    added alarm sounds to component

--- src/components/alarmSound/alarmSound.tsx
+++ src/components/alarmSound/alarmSound.tsx
@@ -1,9 +1,19 @@
 import React,{useState} from 'react';
+import soundOne from './sounds/soundOne.mp3';
+import soundTwo from './sounds/soundTwo.mp3';
+import soundThree from './sounds/soundThree.mp3';
+
 
 const AlarmSound=()=>{
     const [soundValue, setSoundValue]= useState("");
+    // const [audio] = useState(new Audio(soundOne));
     const handleSoundChange=(event:any): void=>{
         setSoundValue(event.target.value)
+    }
+
+    const playSound=(sound: any)=>{
+        const audio= new Audio(sound);
+        audio.play()
     }
     // if soundValue and ... are thesame play ... sound
     return (
@@ -15,6 +25,9 @@ const AlarmSound=()=>{
                 <option value="audi">Audi</option>
             </select>
             <p>{soundValue}</p>
+            <button onClick={()=>playSound(soundOne)}>Alarm</button>
+            <button onClick={()=>playSound(soundTwo)}>Emergency</button>
+            <button onClick={()=>playSound(soundThree)}>Clock ticks</button>
         </div>
     )
 }
